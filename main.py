a9a2e0b64f57fad88611a66a8aa2246f420485e3
LanaOst <light1force@gmail.com>
2021-02-18 09:27:32 +0200

    Added state machine

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram.types.message import ContentType
 from aiogram.utils.markdown import text, bold, italic, code, pre
-from aiogram.types import ParseMode, InputMediaPhoto, InputMediaVideo, ChatActions
+from aiogram.types import ParseMode, InputMediaPhoto, ChatActions
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
 import requests
 
 ##### DELETE in PRODUCTION
@@ -16,15 +20,31 @@ def jprint(obj):
 #####
 
 from conf import TOKEN
+from datetime import datetime
+
 
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
+dp.middleware.setup(LoggingMiddleware())
+
+
+class State(StatesGroup):
+    reqtime = State()
+    req = State()
+
+
+def islimit(ts1, ts2):
+    delta = ts2 - ts1
+    min = delta.total_seconds() / 60
+    return min < 10.00
+    
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply('Привет!\nИспользуй /help, '
                         'чтобы узнать список доступных команд!')
+                        
 
 
 @dp.message_handler(commands=['help'])
@@ -33,24 +53,47 @@ async def process_help_command(message: types.Message):
                '/list', '/lst', '/exchange', '/history', sep='\n')
     await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
 
-@dp.message_handler(commands=['list', 'lst'])
-async def process_photo_command(message: types.Message):
-    response = requests.get("http://api.exchangeratesapi.io/latest?base=USD")
-    ms = ''
-    #### DELETE on Prod
-    #print(response.status_code)
-    #jprint(response.json())
-    ####
-    
-    
-    if response.status_code == 200:
-        rts = response.json()['rates']
-        for i in rts:
-            ms += i+': {:.2f}'.format(rts[i])+'\n'
-            #ms += i + ': '+str(int(( rts[i] * 100 ) + 0.5) / float(100))+'\n'
-        print(ms)
+'''все сообщения при нулевом состоянии
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+   '''
+   
+@dp.message_handler(state = '*', commands=['list', 'lst'])
+async def process_list_command(message: types.Message, state: FSMContext):
+    state = await state.get_state()
+    ms = '' #init ms for message
+    if state is None: # first user input
+        await State.req.set()   # set any state to know that user got request
+        async with state.proxy() as data:
+            data['reqtime'] = datetime.datetime.now().timestamp() #save the time of the request
+            response = requests.get("http://api.exchangeratesapi.io/latest?base=USD")
+            ms = ''
+            if response.status_code == 200:
+                data['req'] = response.json()['rates']
+                for i in data['req']:
+                    ms += i+': {:.2f}'.format(rts[i])+'\n'
+            else: ms = 'Data was not received'
+    else:   # not the first user input
+        now = datetime.datetime.now().timestamp()
+        async with state.proxy() as data:
+            if not islimit(data['reqtime'], now): # 10 min is passed
+                data['reqtime'] = datetime.datetime.now().timestamp() #renew timestamp
+                response = requests.get("http://api.exchangeratesapi.io/latest?base=USD")
+                ms = ''
+                if response.status_code == 200:
+                    data['req'] = response.json()['rates']
+                    for i in data['req']:
+                        ms += i+': {:.2f}'.format(rts[i])+'\n'
+                else: ms = 'Data was not received'
+            else:
+                if data['req'] is not None:
+                    for i in data['req']:
+                        ms += i+': {:.2f}'.format(rts[i])+'\n'
+                else: ms = 'Data was not received'
 
     await message.reply(ms)
+    
     
 @dp.message_handler()
 async def echo_message(msg: types.Message):
